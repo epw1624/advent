e57f292cb856628e09bdedd13a9d528a6042de9d
epw1624 <emily.wood1624@gmail.com>
2024-12-16 18:14:33 -0800

    i <3 numpy

---
+++ day13/part2.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+tolerance = 1e-4
+
+with open("input.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+    machines = [lines[i:i+3] for i in range(0, len(lines), 4)]
+
+    tokens = 0
+
+    for machine in machines:
+        xa = int(machine[0].split(',')[0].split('+')[-1])
+        ya = int(machine[0].split(',')[1].split('+')[-1])
+
+        xb = int(machine[1].split(',')[0].split('+')[-1])
+        yb = int(machine[1].split(',')[1].split('+')[-1])
+
+        prize_x = int(machine[2].split(',')[0].split('=')[-1]) + 10000000000000
+        prize_y = int(machine[2].split(',')[-1].split('=')[-1]) + 10000000000000
+
+        m = np.array([[xa, xb], [ya, yb]])
+        b = np.array([prize_x, prize_y])
+
+        try:
+            x = np.linalg.solve(m, b)
+
+            a, b = x
+
+            if abs(a - round(a)) < tolerance and abs(b - round(b)) < tolerance:
+                tokens += 3 * round(a) + round(b)
+
+        except np.linalg.LinAlgError:
+            pass
+
+    print(tokens)
