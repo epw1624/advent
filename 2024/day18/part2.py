6298228bdde4d8cc0b57606cd90206bc710853f0
epw1624 <emily.wood1624@gmail.com>
2024-12-26 15:41:27 -0800

    day18

---
+++ 2024/day18/part2.py
@@ -0,0 +1,53 @@
+from heapq import heappop, heappush
+
+GRID_SIZE=71
+BYTES_TO_SIMULATE=1024
+DIRECTIONS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+def shortest_path(grid):
+    pq = []
+    heappush(pq, (0, 0, 0))
+    visited = set()
+
+    while pq:
+        l, x, y = heappop(pq)
+        if x == GRID_SIZE - 1 and y == GRID_SIZE - 1:
+            return l
+        
+        if (x, y) in visited:
+            continue
+        else:
+            visited.add((x, y))
+
+        for dx, dy in DIRECTIONS:
+            nx, ny = x + dx, y + dy
+            if nx >= 0 and nx < GRID_SIZE and ny >= 0 and ny < GRID_SIZE and grid[ny][nx] != '#':
+                heappush(pq, (l + 1, nx, ny))
+
+    return None
+
+with open("input.txt") as file:
+    bytes = [line.rstrip() for line in file]
+
+    # initialize empty grid
+    grid = [['.' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+
+    # simulate bytes falling
+    for i in range(BYTES_TO_SIMULATE):
+        byte = bytes[i].split(',')
+        x, y = int(byte[0]), int(byte[1])
+        grid[y][x] = '#'
+
+    # simulate next byte, and check that a path still exists
+    i = BYTES_TO_SIMULATE
+    while True:
+        byte = bytes[i].split(',')
+        x, y = int(byte[0]), int(byte[1])
+        grid[y][x] = '#'
+
+        if not shortest_path(grid):
+            print(x, y)
+            break
+
+        i += 1
+
