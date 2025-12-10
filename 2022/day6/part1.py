a7263adb9e7f55bcfe7e1fa198f1f7f1666f6ea6
epw1624 <emily.wood1624@gmail.com>
2025-12-10 15:49:24 -0800

    remove random stuff

--- 2022/day6/part1.py
+++ 2022/day6/part1.py
@@ -1,7 +1,6 @@
 with open("input.txt") as file:
     content = file.read()
     counter = 3
-    marker = "0000"
     for i in range(len(content)):
         counter += 1
         if len(set(content[i:i+4])) == 4:
--- 2022/day6/part2.py
+++ 2022/day6/part2.py
@@ -1,7 +1,6 @@
 with open("input.txt") as file:
     content = file.read()
     counter = 13
-    marker = "0000"
     for i in range(len(content)):
         counter += 1
         if len(set(content[i:i+14])) == 14:
